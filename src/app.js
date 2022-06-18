4105d461bc2c8cce136e1e242ddd4c1d6634e497
Legebrind <moyi88@gmail.com>
2022-06-18 15:52:39 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  function aleatorio(base) {
+    return Math.floor(Math.random() * base);
+  }
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  var aImprimir =
+    who[aleatorio(4)] +
+    " " +
+    action[aleatorio(4)] +
+    " " +
+    what[aleatorio(3)] +
+    " " +
+    when[aleatorio(5)];
+  document.getElementById("mensaje").innerHTML = aImprimir;
+
   console.log("Hello Rigo from the console!");
 };
